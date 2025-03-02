953c7d022bfbbbba6937a8b51da8f1f5a332ce3b
ljstadler <82152013+ljstadler@users.noreply.github.com>
2025-03-02 09:36:20 +0200

    Update homepage

--- src/main.ts
+++ src/main.ts
@@ -5,20 +5,53 @@ const defaultBang = localStorage.getItem("defaultBang") ?? "ddg";
 
 const homePage = () => {
     document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-    <h3>https://bang-search.pages.dev?q=%s</h1>
-    <div id="settings">
-        <label for="default">Default Bang</label>
-        <input id="default" type="text" value="${defaultBang}" />
-        <button id="save">Save</button>
-    </div>
+        <div style="display: flex; flex-direction: column; align-items: center; gap: 5px;">
+            <div>
+                <label for="default">Default Bang</label>
+                <div style="display: flex; gap: 5px;">
+                    <input id="default" type="text" value="${defaultBang}" />
+                    <button id="save" title="Save">
+                        <img id="save-icon" src="/save.svg" alt="Save" />
+                    </button>
+                </div>
+            </div>
+
+            <div>
+                <label for="search">Search URL</label>
+                <div style="display: flex; gap: 5px;">
+                    <input id="search" type="text" value="https://bang-search.pages.dev?q=%s" disabled />
+                    <button id="copy" title="Copy">
+                        <img id="copy-icon" src="/copy.svg" alt="Copy" />
+                    </button>
+                </div>
+            </div>
+        </div>
 `;
 
     const defaultInput = document.querySelector<HTMLInputElement>("#default")!;
     const saveButton = document.querySelector<HTMLButtonElement>("#save")!;
+    const saveIcon = document.querySelector<HTMLImageElement>("#save-icon")!;
 
     saveButton.addEventListener("click", () => {
         localStorage.setItem("defaultBang", defaultInput.value);
-        window.location.reload();
+
+        saveIcon.src = "/check.svg";
+        setTimeout(() => {
+            saveIcon.src = "/save.svg";
+        }, 2000);
+    });
+
+    const searchInput = document.querySelector<HTMLInputElement>("#search")!;
+    const copyButton = document.querySelector<HTMLButtonElement>("#copy")!;
+    const copyIcon = document.querySelector<HTMLImageElement>("#copy-icon")!;
+
+    copyButton.addEventListener("click", async () => {
+        await navigator.clipboard.writeText(searchInput.value);
+
+        copyIcon.src = "/check.svg";
+        setTimeout(() => {
+            copyIcon.src = "/copy.svg";
+        }, 2000);
     });
 };
 
