a8130eccbbda1948fc48dfee0bf2f0602cfd4874
ljstadler <82152013+ljstadler@users.noreply.github.com>
2025-04-29 09:25:51 +0200

    Redirect to selected bang's homepage if search query is empty Use Google by default

--- src/main.ts
+++ src/main.ts
@@ -72,6 +72,9 @@ const getRedirectUrl = () => {
 
     const cleanQuery = query.replace(/!\S+\s*/i, "").trim();
 
+    if (cleanQuery === "")
+        return selectedBang ? `${new URL(selectedBang).origin}` : null;
+
     const searchUrl = selectedBang.replace(
         "{{{s}}}",
         encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
