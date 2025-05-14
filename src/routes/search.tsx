6729ee4eef57a7af8bad77b8406c18bab035027c
ljstadler <82152013+ljstadler@users.noreply.github.com>
2025-05-14 14:48:03 +0200

    Perform search in route loader

--- src/routes/search.tsx
+++ src/routes/search.tsx
@@ -6,7 +6,6 @@ type Search = {
 };
 
 export const Route = createFileRoute("/search")({
-    component: Search,
     validateSearch: (search: Record<string, unknown>): Search => {
         return {
             q: search.q as string,
@@ -14,22 +13,14 @@ export const Route = createFileRoute("/search")({
     },
     loaderDeps: ({ search: { q } }) => ({ q }),
     loader: ({ deps: { q } }) => {
-        if (!q) {
-            throw redirect({
-                to: "/",
-            });
-        }
+        if (!q) throw redirect({ to: "/" });
+
+        const defaultBang = localStorage.getItem("defaultBang") ?? "g";
+
+        const url = getRedirectUrl(q, defaultBang);
+
+        if (!url) throw redirect({ to: "/" });
+
+        throw redirect({ href: url });
     },
 });
-
-function Search() {
-    const { q } = Route.useSearch();
-
-    const defaultBang = localStorage.getItem("defaultBang") ?? "g";
-
-    const url = getRedirectUrl(q, defaultBang);
-
-    if (!url) throw redirect({ to: "/" });
-
-    location.replace(url);
-}
